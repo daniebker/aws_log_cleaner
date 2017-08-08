779e8ddfa10e2dfa902fe8e421c256ab903be2cf
Victor Jimenez <vjimenez@vistaprint.com>
2017-08-08 15:26:03 +0200

    Fixes #4

--- lib/aws_log_cleaner/api_gateway_retriever.rb
+++ lib/aws_log_cleaner/api_gateway_retriever.rb
@@ -1,16 +1,16 @@
 
 module AwsLogCleaner
 
-  # Responsible for retrieving API gateway instances filtered by prefix
+  # Responsible for retrieving API gateway instances filtered by a given text
   class ApiGatewayRetriever
 
     def initialize(api_gateway)
       @api_gateway = api_gateway
     end
 
-    def retrieve(prefix)
+    def retrieve(text)
       apis = @api_gateway.list_all_apis
-      apis.select{ |item| item.name.to_s.downcase.start_with?(prefix) }
+      apis.select{ |item| item.name.to_s.downcase.include?(text) }
     end
 
   end
